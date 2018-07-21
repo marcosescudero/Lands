4af7601e6e42c887c7bc6e91b075375286bf58f8
Marcos Escudero <marcosescudero@hotmail.com>
2018-07-21 18:35:14 -0300

    version1
    aaa

---
+++ Lands/Lands/App.xaml.cs
@@ -0,0 +1,39 @@
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Lands.Views; // Nuestra carpeta
+
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+namespace Lands
+{
+
+	public partial class App : Application
+	{
+        #region Constructors
+        public App ()
+		{
+			InitializeComponent();
+
+            //MainPage = new MainPage();
+            this.MainPage = new NavigationPage(new LoginPage());
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        } 
+        #endregion
+    }
+}
