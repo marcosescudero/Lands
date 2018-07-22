59906d748b46e5b4e334492328e0e1c41e70c847
Marcos Escudero <marcosescudero@hotmail.com>
2018-07-22 18:45:22 -0300

    Check connection

---
+++ Lands/Lands/Models/Response.cs
@@ -0,0 +1,19 @@
+﻿namespace Lands.Models
+{
+    public class Response
+    {
+        public bool IsSuccess {
+            get;
+            set;
+        }
+        public string Message{
+            get;
+            set;
+        }
+        public object Result {
+            get;
+            set;
+        }
+
+    }
+}
